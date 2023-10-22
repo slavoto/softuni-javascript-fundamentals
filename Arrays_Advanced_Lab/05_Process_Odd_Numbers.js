f2a9359b93f4c9c0e73b1c45628960a01f30829b
Svetoslav <thegameso23@gmail.com>
2023-10-22 12:39:08 +0300

    solve exercise

--- Arrays_Advanced_Lab/05_Process_Odd_Numbers.js
+++ Arrays_Advanced_Lab/05_Process_Odd_Numbers.js
@@ -11,3 +11,9 @@ function processOddNumber(arr) {
 }
 
 processOddNumber([10, 20, 30, 40 , 50, 60]);
+
+// You are given an array of numbers. Write a function that prints the elements at odd positions from the array, doubled and in reverse order.
+
+// The input comes as an array of number elements.
+
+// The output is printed on the console on a single line, separated by space.
