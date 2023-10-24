caf052a791e2622ea6d70988fb7e88f10d3d32af
Svetoslav <thegameso23@gmail.com>
2023-10-25 01:30:01 +0300

    solve exercise

--- Arrays_Advanced_Lab/06. Smallest Two Numbers.js
+++ Arrays_Advanced_Lab/06. Smallest Two Numbers.js
@@ -6,3 +6,9 @@ function smallestTwoNumbers(arr) {
 }
 
 smallestTwoNumbers([30, 15, 50, 5]);
+
+// Write a function that prints the two smallest elements from an array of numbers.
+
+// The input comes as an array of number elements.
+
+// The output is printed on the console on a single line, separated by space.
