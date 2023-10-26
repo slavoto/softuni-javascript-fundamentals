c55550552616cf988108262ab626005d903749e8
Svetoslav <thegameso23@gmail.com>
2023-10-27 00:26:51 +0300

    solve arrays advanced

--- Arrays_Advanced_Exercise/03_House Party.js
+++ Arrays_Advanced_Exercise/03_House Party.js
@@ -1,6 +1,29 @@
-function houseParty() {
+function houseParty(arr) {
 
-    
+    let guests = [];
+
+    for(let command of arr) {
+        let tokens = command.split(' ');
+        let name = tokens[0];
+
+        if(tokens.includes('not')) {
+            if(guests.includes(name)) {
+                let index = guests.indexOf(name);
+                guests.splice(index, 1);
+            } else {
+                console.log(`${name} is not in the list!`);
+            }
+            
+        }
+        else {
+            if(guests.includes(name)) {
+                console.log(`${name} is already in the list!`);
+            } else {
+                guests.push(name);
+            }
+        }
+    }
+    console.log(guests.join('\n'));
 }
 
 houseParty(['Allie is going!',
@@ -10,3 +33,19 @@ houseParty(['Allie is going!',
 'John is not going!',
 
 'George is not going!']);
+
+// Write a function that keeps track of guests that are going to a house party.
+
+// You will be given an array of strings. Each string will be one of the following:
+
+// - "{name} is going!"
+
+// - "{name} is not going!"
+
+// If you receive the first type of input, you have to add the person if he/she is not in the list 
+// (If he/she is in the list print: "{name} is already in the list!").
+
+// If you receive the second type of input, you have to remove the person if he/she is in the list 
+// (if not print: "{name} is not in the list!").
+
+// At the end print all the guests each on a separate line.
