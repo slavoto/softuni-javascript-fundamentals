52b4dd19659b405504768d0a2eba5448dbba34d0
Svetoslav <thegameso23@gmail.com>
2023-10-31 00:08:45 +0200

    solve exercise arrays advanced

--- Arrays_Advanced_Exercise/07_Search_for_a_Number.js
+++ Arrays_Advanced_Exercise/07_Search_for_a_Number.js
@@ -1,6 +1,20 @@
-function searchNumber() {
+function searchNumber(nums, criteria) {
+    let count = 0;
+    let el = criteria[2];
 
+    let arr = nums.slice(0, criteria[0]);
+    // here
+    arr = arr.slice(criteria[1], arr.length);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === el) {
+            count++;
+        }
+    }
     
+
+    console.log(`Number ${el} occurs ${count} times.`);
+
 }
 
 searchNumber([5, 2, 3, 4, 1, 6], [5, 2, 3]);
