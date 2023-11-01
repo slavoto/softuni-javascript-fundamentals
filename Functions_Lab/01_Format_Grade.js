05ed011759bbd07abdefea938d6c4a0bbe628d67
Svetoslav <thegameso23@gmail.com>
2023-11-01 23:48:17 +0200

    solve function

--- Functions_Lab/01_Format_Grade.js
+++ Functions_Lab/01_Format_Grade.js
@@ -20,3 +20,15 @@ function formatGrade(grade) {
 formatGrade(3.33);
 formatGrade(4.50);
 formatGrade(2.99);
+
+// function that receives a grade between 2.00 and 6.00 and prints a formatted line with grade and description.
+
+// · < 3.00 - "Fail"
+
+// · >= 3.00 and < 3.50 - "Poor"
+
+// · >= 3.50 and < 4.50 - "Good"
+
+// · >= 4.50 and < 5.50 - "Very good"
+
+// · >= 5.50 - "Excellent"
