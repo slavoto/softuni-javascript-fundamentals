5c625f4e22a04868004824355dc4e03544d85773
Svetoslav <thegameso23@gmail.com>
2023-11-03 01:53:07 +0200

    solve functions lab

---
+++ Functions_Lab/05_Simple_Calculator.js
@@ -0,0 +1,21 @@
+function simpleCalculator(a, b, operator) {
+
+    if(operator == 'add') {
+        console.log(a + b);
+    } else if(operator == 'subtract') {
+            console.log(a - b);
+    } else if(operator == 'multiply') {
+        console.log(a * b);
+    } else if(operator == 'devide') {
+        console.log(a / b);
+    }
+}
+
+
+simpleCalculator(5, 5, 'multiply');
+simpleCalculator(40, 8, 'divide');
+
+// Write a function that receives three parameters – two numbers and an operator (string) – 
+// and calculates the result depending on the operator. 
+// Operator can be 'multiply', 'divide', 'add' or 'subtract'. 
+// Try to solve this task using arrow functions.
