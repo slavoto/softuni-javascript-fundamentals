e96a36f0c78e3ebd9516d39c38c594e2c4fb4e05
Svetoslav <thegameso23@gmail.com>
2023-11-30 01:12:07 +0200

    solve objects and classes exercise

---
+++ Objects_and_Classes_Exercise/02_Towns.js
@@ -0,0 +1,24 @@
+function town(array) {
+
+    for(let town of array) {
+        let [ townName, latitude, longitude] = town.split(' | ');
+        latitude = Number(latitude).toFixed(2);
+        longitude = Number(longitude).toFixed(2);
+
+        let townObj = {town: townName, latitude: latitude, longitude: longitude};
+        console.log(townObj);
+    }
+
+}
+
+town(['Sofia | 42.696552 | 23.32601',
+
+'Beijing | 39.913818 | 116.363625']);
+
+// You’re tasked to create and print objects from a text table.
+
+// You will receive the input as an array of strings, where each string represents a table row, with values on the row separated by pipes " | " and spaces.
+
+// The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude". The latitude and longitude columns will always contain valid numbers. Check the examples to get a better understanding of your task.
+
+// The output should be objects. Latitude and longitude must be parsed to numbers and formatted to the second decimal point!
