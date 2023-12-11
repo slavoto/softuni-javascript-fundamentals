032e05b5dcd84dc4bf2ce7795e8fc619dee0297e
Svetoslav <thegameso23@gmail.com>
2023-12-11 03:21:28 +0200

    solve associative arrays exercise

---
+++ Assoc_Arrays_Exercise/01_Word_Tracker.js
@@ -0,0 +1,44 @@
+function wordTracker(input) {
+
+    let searchedWords = input.shift().split(' ');
+
+    let occurrences = {};
+
+    for(let word of searchedWords) {
+        occurrences[word] = 0;
+    }
+
+    for(let word of input) {
+        if(word in occurrences) {
+            occurrences[word]++;
+        }
+    }
+
+    let entries = Object.entries(occurrences).sort((a,b) => b[1] - a[1]);
+
+    for(let [word, repeats] of entries) {
+        console.log(word, '-', repeats);
+    }
+    
+}
+
+wordTracker([
+
+    'this sentence',
+    
+    'In', 'this', 'sentence', 'you', 'have',
+    
+    'to', 'count', 'the', 'occurrences', 'of',
+    
+    'the', 'words', 'this', 'and', 'sentence',
+    
+    'because', 'this', 'is', 'your', 'task'
+    
+    ]);
+
+// Write a function that receives an array of words and finds occurrences of given words in that sentence.
+
+// The input will come as an array of strings. The first string will contain the words you will be looking for separated by a space. 
+// All strings after that will be the words in which you will check for a match.
+
+// Print for each word how many times it occurs. The words should be sorted by count in descending.
